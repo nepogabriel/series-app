6ced4d7f255ae9d0ae11bcd3b9c8c47f0e9a2921
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2024-05-10 11:25:14 -0300

    Visualizando episódios

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
@@ -37,3 +38,8 @@ Route::controller(SeriesController::class)->group(function() {
 }); */
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('seasons/{season}/episodes', function(\Illuminate\Http\Request $request) {
+    dd($request->all());
+});
